4c59d7982e7060785abed5b82ad18f92a5e2bfc6
A WILD DADDY <cispeo2000@gmail.com>
2022-02-02 14:51:18 +0100

    start_player_guess_game, show, hide, new_game done

--- js/nombre-mystere.js
+++ js/nombre-mystere.js
@@ -1,9 +1,19 @@
-var min = 0;
-var max = 100;
-var computer_number = Math.floor(Math.random() * (max - min)) + min;
-alert(computer_number);
-
 function start_player_guess_game() {
-    
+    var min = 1;
+    var max = 100;
+    var computer_number = Math.floor(Math.random() * (max - min)) + min;
+}
 
+function show(element) {
+    element.style.display = ’’;
+}
+
+function hide(element) {
+    element.style.display = ’none’;
+}
+
+function new_game() { // fonction qui montre l’´ecran de s´election du mode de jeu
+    show(game_selection);
+    hide(game1);
+    hide(game2);
 }
