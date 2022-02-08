583293c33172ddd9f28f5b9264f9eccb2385baee
A WILD DADDY <cispeo2000@gmail.com>
2022-02-08 19:50:36 +0100

    END

--- js/nombre-mystere.js
+++ js/nombre-mystere.js
@@ -1,19 +1,91 @@
-function start_player_guess_game() {
-    var min = 1;
-    var max = 100;
-    var computer_number = Math.floor(Math.random() * (max - min)) + min;
+let computer_value;
+let min=0;
+let max=101;
+computer_value = 50;
+
+document.addEventListener('DOMContentLoaded',Main);
+
+function Main(event){
 }
 
-function show(element) {
-    element.style.display = ’’;
+function show(element){
+    element.style.display = '';
 }
 
-function hide(element) {
-    element.style.display = ’none’;
+function hide(element){
+    element.style.display='none';
 }
 
-function new_game() { // fonction qui montre l’´ecran de s´election du mode de jeu
-    show(game_selection);
-    hide(game1);
-    hide(game2);
+function new_game(){
+    show(gameSelection);
+    hide(Game_Player);
+    hide(Game_Computer);
+    Game1result.innerHTML=''
+    Game2result.innerHTML="Je trouverais ton nombre"
+    player_input.value='';
 }
+
+new_game();
+
+function start_player_guess_game(){
+    computer_value = Math.floor((Math.random()*100)+1)
+    hide(gameSelection);
+    show(Game_Player);
+    hide(Game_Computer);
+    hide(restart1);
+    trying.addEventListener('click',(event)=>{try_guess()});
+    restart1.addEventListener('click',(event)=>{location.reload()});
+}
+
+function try_guess(){
+    input = player_input.value;
+    if(input > computer_value){
+        Game1result.innerHTML="Non c'est plus petit";
+        player_input.value='';
+    }
+    else if(input < computer_value){
+        Game1result.innerHTML="non c'est plus Grand";
+        player_input.value='';
+    }
+    else{
+        Game1result.innerHTML= "Bravo, tu as trouvé !!";
+        show(restart1);
+    }
+}
+
+function its_smaller(){
+    max = computer_value;
+    computer_value = Math.floor((min+max)/2);
+    Game2result.innerHTML='est ce que '+ computer_value +' est ton nombre ?';
+}
+
+function its_bigger(){
+    min = computer_value;
+    computer_value = Math.floor((min+max)/2);
+    Game2result.innerHTML='est ce que '+ computer_value +' est ton nombre ?';
+}
+
+function start_computer_guess_game(){
+    hide(gameSelection);
+    hide(Game_Player);
+    show(Game_Computer);
+    hide(restart2);
+    show(small);
+    show(bigger);
+    show(found);
+
+    Game2result.innerHTML='est ce que '+ computer_value +' est ton nombre ?'
+    small.addEventListener('click',(event)=>{its_smaller()});
+    bigger.addEventListener('click',(event)=>{its_bigger()});
+    restart2.addEventListener('click',(event)=>{location.reload()});
+    found.addEventListener('click',(event)=>{
+        Game2result.innerHTML="J'ai trouvé";
+        show(restart2);
+        hide(small);
+        hide(bigger);
+        hide(found)});
+}
+
+
+game1.addEventListener('click',(event)=>{start_player_guess_game()});
+game2.addEventListener('click',(event)=>start_computer_guess_game());
